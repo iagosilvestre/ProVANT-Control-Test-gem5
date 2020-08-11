43c2ccb05441dbb3453d5b7c2cb4b6d6f5478826
iago notebook <iagosilvestrehp@hotmail.com>
2020-08-11 19:07:25 -0300

    no cache and rasp4 results

--- vant2_lqr/vant2lqr.cpp
+++ vant2_lqr/vant2lqr.cpp
@@ -18,7 +18,7 @@
 
 int main()
 	{
-	m5_reset_stats(0,0);
+	//m5_reset_stats(0,0);
 	simulator_msgs::SensorArray arraymsg;
 	std::vector<double> out;
 	std::vector<double> xref;
@@ -43,12 +43,12 @@ int main()
 //		msgstates = arraymsg.values.at(0);
 		
 		while(k<20){
-		//m5_reset_stats(0,0);
+		m5_reset_stats(0,0);
 		out=control->execute(arraymsg);
-		//m5_dump_stats(0,0);
+		m5_dump_stats(0,0);
 
 		k++;
 		}
-m5_dump_stats(0,0);
+//m5_dump_stats(0,0);
 		return 0;
 	}
