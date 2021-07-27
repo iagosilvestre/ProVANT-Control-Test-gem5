db41f404721305c25fae74fdbebf12a09972f578
iago notebook <iagosilvestrehp@hotmail.com>
2021-07-27 17:48:14 -0300

    all

---
+++ prefetch/vant4_winf/vant4winf.cpp
@@ -0,0 +1,71 @@
+#include "Icontroller.hpp"
+#include <iostream>
+#include <Eigen/Eigen>
+#include "include/Sensor.h"
+#include "include/SensorArray.h"
+#include "math.h"
+#include "winf.cpp"
+#include <algorithm>
+#include <chrono>
+#include <vector>
+#include <memory>
+#include <string>
+#include "m5op.h"
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+
+
+
+int main()
+	{
+	m5_reset_stats(0,0);
+	std::vector<int> controlData;
+	unsigned long int auxCount=0;
+
+	simulator_msgs::SensorArray arraymsg;
+	std::vector<double> out;
+	std::vector<double> xref;
+	std::vector<double> error;
+	std::vector<double> x;
+	int k=0;
+
+
+//		Foo* foo1 = new Foo ();
+//		std::cout << "test if main works" << std::endl;;
+		vant4_Winf* control = new vant4_Winf();
+
+
+
+		control->config();
+
+
+//		simulator_msgs::Sensor msgstates;
+//		arraymsg.header;
+//		msgstates = arraymsg.values.at(0);
+		//auto all = std::chrono::high_resolution_clock::now();
+		while(k<100){
+		//auto start = std::chrono::high_resolution_clock::now();
+		//m5_reset_stats(0,0);
+		out=control->execute(arraymsg);
+		//m5_dump_stats(0,0);
+		//auto elapsed = std::chrono::high_resolution_clock::now() - start;
+		//long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+		//controlData.push_back(microseconds);
+		k++;
+		}
+		//auto elapsed = std::chrono::high_resolution_clock::now() - all;
+		//long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+		//controlData.push_back(microseconds);
+			//FILE *fCON = fopen("control.txt", "w");
+			//	fprintf(fCON, "count;dtCon\n");
+			//	fclose(fCON);
+			//	for (std::vector<int>::iterator it = controlData.begin() ; it != controlData.end(); ++it){
+			//		auxCount++;
+			//		FILE *fCON = fopen("control.txt", "a");
+			//		fprintf(fCON, "%d;%lu\n",auxCount,*it);
+			//		fclose(fCON);
+			//	}
+			m5_dump_stats(0,0);
+		return 0;
+	}
